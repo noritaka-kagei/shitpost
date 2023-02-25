59942a92945f626d7ad58ef14dbd6ad40156ca44
noritaka-kagei <it0ka4ge8i@gmail.com>
2023-02-25 15:11:10 +0900

    add controller and view page used Thymeleaf template engine

---
+++ sample-web/src/main/java/com/noritakakagei/sample/controller/HelloModelController.java
@@ -0,0 +1,17 @@
+package com.noritakakagei.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("hello")
+public class HelloModelController {
+
+    @GetMapping("model")
+    public String helloView(Model model){
+        model.addAttribute("message", "It works on Thymeleaf!! :)");
+        return "helloThymeleaf";
+    }
+}
