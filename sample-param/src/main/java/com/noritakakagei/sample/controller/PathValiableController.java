cfb1ae7cb0fcee18ed52966d59da4e775a80c6a0
keronori <it0ka4ge8i@gmail.com>
2023-02-28 20:48:51 +0900

    add parameter query request handling

--- sample-param/src/main/java/com/noritakakagei/sample/controller/PathValiableController.java
+++ sample-param/src/main/java/com/noritakakagei/sample/controller/PathValiableController.java
@@ -4,9 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PathValiableController {
+
+    @GetMapping("/parameter")
+    @ResponseBody
+    public String showParamResponse(@RequestParam String response) {
+        // HTTP Access to /parameter?response=hoge -> return "hoge"
+        return response;
+    }
 
     @GetMapping("/parameter/{param}")
     public String showParam(Model model, @PathVariable String param) {
