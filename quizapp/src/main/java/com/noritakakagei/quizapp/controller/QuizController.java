eb351f38e036ea33027fcf120a7dce257afbe13d
noritaka-kagei <it0ka4ge8i@gmail.com>
2023-03-06 00:26:13 +0900

    implement Create request handler

--- quizapp/src/main/java/com/noritakakagei/quizapp/controller/QuizController.java
+++ quizapp/src/main/java/com/noritakakagei/quizapp/controller/QuizController.java
@@ -3,9 +3,13 @@ package com.noritakakagei.quizapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.noritakakagei.quizapp.entity.Quiz;
 import com.noritakakagei.quizapp.form.QuizForm;
@@ -33,5 +37,27 @@ public class QuizController {
         model.addAttribute("title", "Register Form");
 
         return "crud";
+    }
+
+    @PostMapping("/insert")
+    public String insert(@Validated QuizForm form, 
+        BindingResult bindingResult, 
+        Model model, 
+        RedirectAttributes redirectAttributes) {
+
+            if (bindingResult.hasErrors()) {
+                return index(form, model);
+            }
+
+            // Form -> Quiz (instance)
+            Quiz quiz = new Quiz();
+            quiz.setQuestion(form.getQuestion());
+            quiz.setAnswer(form.getAnswer());
+            quiz.setAuthor(form.getAuthor());
+
+            // register the quiz to Database
+            service.insertQuiz(quiz);
+            redirectAttributes.addFlashAttribute("complete", "Successfully registered the quiz.");
+            return "redirect:/quiz";
     }
 }
