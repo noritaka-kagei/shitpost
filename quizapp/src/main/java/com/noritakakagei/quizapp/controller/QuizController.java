bf2c6f59d17dc616aa1471e1c3d21c250b5cc964
noritaka-kagei <it0ka4ge8i@gmail.com>
2023-03-06 19:40:31 +0900

    remove extra import statement

--- quizapp/src/main/java/com/noritakakagei/quizapp/controller/QuizController.java
+++ quizapp/src/main/java/com/noritakakagei/quizapp/controller/QuizController.java
@@ -2,7 +2,6 @@ package com.noritakakagei.quizapp.controller;
 
 import java.util.Optional;
 
-import org.apache.catalina.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
